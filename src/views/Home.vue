6bf13dacce06b8165908ee098c2f9b4bbcb4a145
liuyan36 <616098210@qq.com>
2020-09-21 16:06:24 +0800

    接口写法

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    123
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { getHomeMultidata } from '../api/home'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+  },
+  created() {
+     this.getHomeMultidata()
+  },
+  methods: {
+    getHomeMultidata() {
+      getHomeMultidata().then(res => {
+        console.log(res)
+      })
+    }
   }
 }
 </script>
